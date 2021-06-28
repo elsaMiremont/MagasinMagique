841f8ece525071771d5643f7099ff7b950facb2f
Elsa MIREMONT <elsa.miremont@Digital-Grenoble.local>
2021-06-28 16:10:35 +0200

    Tests finished and green

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
     // Règles censées être déjà implémentées
     @Test
-    void ifSellInDateIsPassedQualityShouldDecreaseTwoTimesFaster()
+    void if_sell_in_date_is_passed_quality_should_decrease_two_times_faster()
     {
-        Item item = new Item("Breloque", 10, 6);
+        Item item = new Item("Breloque", 0, 6);
         Item[] items = new Item[] { item };
         Magasin magasin = new Magasin(items);
         magasin.updateQuality();
@@ -18,7 +18,8 @@ class MagasinTest {
     }
 
     @Test
-    void qualityCannotBeNegative() {
+    void quality_cannot_be_negative()
+    {
         Item item = new Item("Pass VIP Concert", 0, 0);
         Item[] items = new Item[] { item };
         Magasin magasin = new Magasin(items);
@@ -27,13 +28,13 @@ class MagasinTest {
     }
 
     @Test
-    void comteShouldIncreaseItsQuality() {
-        Item comte = new Item("Comté", 12, 10);
+    void comte_quality_should_increase_when_sellin_is_under_10()
+    {
+        Item comte = new Item("Comté", 12, 8);
         Item[] items = new Item[] { comte };
         Magasin magasin = new Magasin(items);
         magasin.updateQuality();
-        assertEquals(11, comte.sellIn);
-        assertEquals(11, comte.quality);
+        assertEquals(9, comte.quality);
     }
 
     @Test
@@ -46,7 +47,8 @@ class MagasinTest {
     }
 
     @Test
-    void kryptoniteNeverChange() {
+    void kryptonite_never_changes()
+    {
         Item item = new Item("Kryptonite", 0, 80);
         Item[] items = new Item[] { item };
         Magasin magasin = new Magasin(items);
@@ -55,13 +57,52 @@ class MagasinTest {
     }
 
     @Test
-    void comteAndPassVIPIncreaseQuality() {
-        Item comte = new Item("Comté", 11, 10);
-        Item pass = new Item("Pass VIP Concert", 11, 15);
+    void comte_and_pass_VIP_increase_quality()
+    {
+        Item comte = new Item("Comté", 2, 9);
+        Item pass = new Item("Pass VIP Concert", 1, 15);
         Item[] items = new Item[] { comte, pass };
         Magasin magasin = new Magasin(items);
         magasin.updateQuality();
-        assertEquals(12, comte.quality);
-        assertEquals(17, pass.quality);
+        assertEquals(10, comte.quality);
+        assertEquals(16, pass.quality);
+    }
+
+
+    @Test
+    void vip_concert_quality_increase_by_three_if_time_is_equal_or_under_5_days ()
+    {
+        Item vipPass = new Item("Pass VIP Concert", 5, 10);
+        Item[] items = new Item[] { vipPass};
+        Magasin shop = new Magasin(items);
+        shop.updateQuality();
+        assertEquals(13, vipPass.quality );
+    }
+
+    @Test
+    void pass_vip_concert_quality_should_fall_to_0_after_sell_in_date ()
+    {
+        Item pass = new Item("Pass VIP Concert", 0, 15);
+        Item[] items = new Item[] { pass };
+        Magasin magasin = new Magasin(items);
+        magasin.updateQuality();
+        assertEquals(0, pass.quality);
+    }
+
+    @Disabled
+    @Test
+    void magic_powers_quality_should_decrease_two_times_faster()
+    {
+        Item power10 = new Item("Pouvoirs magiques", 10, 15);
+        Item[] items10 = new Item[] { power10 };
+        Magasin magasin10 = new Magasin(items10);
+        magasin10.updateQuality();
+        assertEquals(13, power10.quality);
+
+        Item power0 = new Item("Pouvoirs magiques", 0, 15);
+        Item[] items0 = new Item[] { power0 };
+        Magasin magasin0 = new Magasin(items0);
+        magasin0.updateQuality();
+        assertEquals(11, power0.quality);
     }
 }
