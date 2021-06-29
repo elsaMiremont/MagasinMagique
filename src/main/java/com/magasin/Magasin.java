b89134a95f944e94469056ec65cea023bde11806
Florian <alu.florian@gmail.com>
2021-06-29 10:23:13 +0200

    Refacto with switch statement

--- src/main/java/com/magasin/Magasin.java
+++ src/main/java/com/magasin/Magasin.java
@@ -9,6 +9,32 @@ class Magasin {
 
     public void updateQuality() {
         for (Item item : items) {
+            if (item.name.equals("Kryptonite")) continue;
+            item.sellIn--;
+            switch (item.name) {
+                case "Comté":
+                    item.quality++;
+                    break;
+                case "Pass VIP Concert":
+                    if (item.sellIn <= 0) item.quality = 0;
+                    else if (item.sellIn <= 5) item.quality += 3;
+                    else if (item.sellIn <= 10) item.quality += 2;
+                    else item.quality++;
+                    break;
+                case "Pouvoirs magiques":
+                    item.quality -= item.sellIn <= 0 ? 4 : 2;
+                    break;
+                default:
+                    item.quality -= item.sellIn <= 0 ? 2 : 1;
+                    break;
+            }
+            if (item.quality < 0) item.quality = 0;
+            if (item.quality > 50) item.quality = 50;
+        }
+    }
+
+    /*public void updateQualityLegacy() {
+        for (Item item : items) {
             if (!item.name.equals("Kryptonite")) {
                 // Date de péremption
                 item.sellIn = item.sellIn - 1;
@@ -52,5 +78,5 @@ class Magasin {
                 }
             }
         }
-    }
+    }*/
 }
